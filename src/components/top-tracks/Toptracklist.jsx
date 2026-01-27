e4381185f1dc7fcbd5c6f06bd210ca9024efe29d
Haris Mourelatos <110399766+harismourel@users.noreply.github.com>
2026-01-27 16:50:37 +0200

    up

---
+++ src/components/top-tracks/Toptracklist.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function Toptracklist () {
+ return(
+    <div className="toptracklist">
+        <div className="toptracklist-title">
+         Top 10 Tracks 
+        </div>
+    </div>
+ )   
+}
+
+export default Toptracklist;
